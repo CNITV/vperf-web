091222ce9876e46d63dfda77df0a68f841350c54
Ciprian Ionescu <me@ciprian-ionescu.com>
2018-06-03 14:40:58 +0300

    Fullscreen & autoscroll

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,9 +7,10 @@ const Header = () => (
 		<h1>Performante Vianiste</h1>
 		<nav>
 			<Link activeClassName={style.active} href="/">Acasa</Link>
-			<Link activeClassName={style.active} href="/profile">Echipe</Link>
-			<Link activeClassName={style.active} href="/profile/john">Probleme</Link>
-			<Link activeClassName={style.active} href="/profile/john">Detalii</Link>
+			<Link activeClassName={style.active} href="/teams">Echipe</Link>
+			<Link activeClassName={style.active} href="/problems">Probleme</Link>
+			<Link activeClassName={style.active} href="/details">Detalii</Link>
+			<Link activeClassName={style.active} href="/admin">Admin</Link>
 		</nav>
 	</header>
 );
--- src/routes/teams/index.js
+++ src/routes/teams/index.js
@@ -2,7 +2,8 @@ import { h, Component } from 'preact';
 //import style from './style';
 import { Button, Segment, Container } from 'semantic-ui-react';
 import { API_URL } from '../../config.js';
-import Scoreboard from '../../components/scoreboard'
+import Scoreboard from '../../components/scoreboard';
+import Fullscreen from 'react-full-screen';
 
 export default class Teams extends Component {
 	constructor(props) {
@@ -10,7 +11,8 @@ export default class Teams extends Component {
 		this.state = {
 			status: {
 				teams: []
-			}
+			},
+			fullscreen: false
 		};
 	}
 
@@ -29,12 +31,27 @@ export default class Teams extends Component {
 		console.log(this.state.status.teams);
 	}
 
+	goFull = () => {
+		this.setState({fullscreen: !this.state.fullscreen});
+	}
+
 	render() {
 		return (
-			<Segment raised padded>
-				<h1>Echipe</h1>
-				<Scoreboard board={this.state.status.teams} />
-			</Segment>
+			<Fullscreen
+				enabled={this.state.fullscreen}
+				onChange={f => this.setState({fullscreen: f})}
+			>
+				<div class='another-container'>
+					<Segment raised padded>
+						<button onClick={this.goFull}>
+							Go Fullscreen
+						</button>
+						<h1>Echipe</h1>
+						<Scoreboard board={this.state.status.teams} />
+					
+					</Segment>
+				</div>
+			</Fullscreen>
 		);
 	}
 }
